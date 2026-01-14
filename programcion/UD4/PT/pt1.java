56bd589b4ab7cbd3169d9d99967cf5338699b794
GaunaRocioAnahi <gaunarocioanahi001.alu@stemgranada.com>
2026-01-14 15:19:06 +0100

    Modificacion y organizacion de archivos por directorios de apuntes

--- programcion/UD4/PT/pt1.java
+++ programcion/UD4/PT/pt1.java
@@ -3,35 +3,10 @@ import java.util.Scanner;
 public class pt1 {
 
     public static void main ( String[] args) {
-        
-        Scanner scanner = new Scanner (System.in);
-        System.out.print("Numero de encargos: ");
-        int numeroEncargos = Scanner.nextInt();
-        scanner.nextLine();
-        while (numeroEncargos <= 0) {
-            System.out.print("Numero de encargos (>0) : ");
-            numeroEncargos = Scanner.nextInt(); 
-        }
-        
-        System.out.print( "Stock de maderas :");
-        int stockMadera = Scanner.nextInt();
-        While (stockMadera <= 0) {
-            System.out.print( "Stock de maderas (>0) :");
-            stockMadera = Scanner.nextInt();
-        }
-        
-        System.out.print("Stock de acero (kg): ");
-        double stockAcero = Scanner.nextDouble();
-        while (stockAcero <= 0) {
-            System.out.print("Stock de acero (> 0): ");
-            stockAcero = Scanner.nextDouble();
-        }
+        Scanner scanner = new Scanner(System.in);
+       // variables
+        int numeroEncargos;
 
-        double maderaGatada = 0, maderaGastadaAcumulada = 0;
-        int aceroGastado = 0, aceroGastadoAcumulado = 0;
-        double importe = 0;
-        int stockMaderaRestante = 0, stockAceroRestante = 0;
-        int flechasVendidas = 0, escudosVendidos = 0, espadasVendidas = 0;
 
 
 
