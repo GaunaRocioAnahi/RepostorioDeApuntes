29cea430e6adf81aa7c87c91a329bbfa8c4b491c
GaunaRocioAnahi <gaunarocioanahi001.alu@stemgranada.com>
2026-01-19 14:16:36 +0100

    ejercicio5 de java R3

--- programcion/UD4/UD1R3/ejercicio5.java
+++ programcion/UD4/UD1R3/ejercicio5.java
@@ -4,7 +4,16 @@ import java.util.Scanner;
 
 public class ejercicio5 {
     public static void main(String[] args) {
-        Scanner.scanner = new scanner(System.in);
-        
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+
+        System.out.print("Ingrese un numero: ");
+        numero = scanner.nextInt(); 
+        scanner.close();
+        if( numero >=10 && numero <=20){
+            System.out.println("El nuermo esta en el Rango" + " "+ numero);
+        } else{
+            System.out.println(" El numero esta fuera del rango" + " " + numero);
+        }
     }
 }
