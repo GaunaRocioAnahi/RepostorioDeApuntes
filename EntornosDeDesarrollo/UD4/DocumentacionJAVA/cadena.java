cf44fb6bc3632936d61f87c31ab8c9d05d5a65f7
GaunaRocioAnahi <gaunarocioanahi001.alu@stemgranada.com>
2026-01-26 15:40:23 +0100

    eliminar codigo de archivo java cadena

--- EntornosDeDesarrollo/UD4/DocumentacionJAVA/cadena.java
+++ EntornosDeDesarrollo/UD4/DocumentacionJAVA/cadena.java
@@ -1,19 +1,3 @@
 public class cadena {
-    /**
-     * Metodo que invierte una cadena de texto.
-     * 
-     * 
-     */
-    public static String revertirCadena(String cadenaTexto) {
-        int cadena = cadenaTexto.length();
-
-        String cadenaInvertida="";
-        for (int i = 1; i < cadena ; i++){
-            cadenaInvertida += cadenaTexto.charArt(cadena -i);
-        }
-        return cadenaInvertida;
-    }
-public static void main(String[] args) {
-    System.out.println(revertirCadena("hola"));
-}
+ 
 }
