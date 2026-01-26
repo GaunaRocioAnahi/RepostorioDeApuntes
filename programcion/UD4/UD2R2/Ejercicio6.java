b42da4718a515fce6902a30ffb61e0f4b01e8adf
GaunaRocioAnahi <gaunarocioanahi001.alu@stemgranada.com>
2026-01-26 18:07:59 +0100

    añadir ejercicio 6 de java

---
+++ programcion/UD4/UD2R2/Ejercicio6.java
@@ -0,0 +1,25 @@
+package programcion.UD4.UD2R2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Ejercicio6 {
+    public static void mostrarTarea(ArrayList<String> listaTarea, ArrayList<Boolean> tareasCompletadas,
+            boolean mostrarTarea) {
+
+    }
+
+    public static void mostrarMensaje(String mesaje) {
+        System.out.println(mensaje);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Arrays
+        ArrayList<String> tarea = new ArrayList<>(Arrays.asList("tarea1", "tarea2", "tarea3"));
+        ArrayList<Boolean> completada = new ArrayList<>(Arrays.asList(false, true, false));
+        int opcion = 0;
+        String buscarTarea ; 
+
+    }
+}
