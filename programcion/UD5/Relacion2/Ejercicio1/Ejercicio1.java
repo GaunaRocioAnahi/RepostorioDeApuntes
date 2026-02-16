8cb3b5089e3fb606953d498011886fe713b7e25b
GaunaRocioAnahi <gaunarocioanahi001.alu@stemgranada.com>
2026-02-16 17:55:04 +0100

    añadir archivos de la relacion 2 UD5 junto con el faker

--- programcion/UD5/Relacion2/Ejercicio1/Ejercicio1.java
+++ programcion/UD5/Relacion2/Ejercicio1/Ejercicio1.java
@@ -9,8 +9,7 @@ public class Ejercicio1 {
         System.out.println("alfredo");
         Estudiante cristobal = new Estudiante("Cristobal", 22, "STEM002");
         Estudiante tomas = new Estudiante("Tomas", 55, "STEM003");
-
-
+        Estudiante estudianteABuscar;
         alfredo.setNombre("alfredo");
         System.out.println(alfredo.getNombre());
         alfredo.setEdad(10);
@@ -20,5 +19,26 @@ public class Ejercicio1 {
         System.out.println(ugr);
         ugr.agregarEstudiante(alfredo);
         ugr.agregarEstudiante(cristobal);
+
+        System.out.println(ugr.mostrarEstudiantes());
+        System.out.println("***************");
+        System.out.println(ugr.mostrarEstudianteCompleto());
+
+        Universidad uma = new Universidad();
+        uma.agregarEstudiante(cristobal);
+        uma.agregarEstudiante(tomas);
+
+        estudianteABuscar = ugr.buscarEstudiante("STEM001");
+        System.out.println(estudianteABuscar);
+
+        Universidad uco = new Universidad();
+        Estudiante estudianteAleatorio;
+        final int NUMEROESTUDIANTES = 100;
+        // aca importamos en este for el archivo faker faker.nombreCompleto
+
+        for(int i = 0; i < NUMEROESTUDIANTES; i++) {
+            estudianteAleatorio = new Estudiante(null, i, null)
+        }
+
     }
 }
