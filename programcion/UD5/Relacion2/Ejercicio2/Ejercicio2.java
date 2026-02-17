034c338f895cd3c2621183defce5976b6a65a748
GaunaRocioAnahi <gaunarocioanahi001.alu@stemgranada.com>
2026-02-17 15:31:36 +0100

    wip : añadir ejercicio 2 de java, relacion 2 de UD5

---
+++ programcion/UD5/Relacion2/Ejercicio2/Ejercicio2.java
@@ -0,0 +1,5 @@
+package programcion.UD5.Relacion2.Ejercicio2;
+
+public class Ejercicio2 {
+    
+}
---
+++ programcion/UD5/Relacion2/Ejercicio2/Libro.java
@@ -0,0 +1,24 @@
+package programcion.UD5.Relacion2.Ejercicio2;
+
+public class Libro {
+
+    //atributos de Libros
+    String id;
+    String titulo;
+    String autor;
+    String sinopsis;
+    Boolean disponible;
+
+    Libro(String id, String titulo, String autor, String sinopsis, Boolean diponible) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.sinopsis = sinopsis;
+        this.disponible = disponible;
+    }
+
+    //Etiqueta 
+    @Override
+
+
+}
